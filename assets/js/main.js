6fa99440c6adf4c62413aa2d6c58f4ee7df1f386
Danofun <danofun@icloud.com>
2018-04-28 04:08:03 -0400

    use AJAX for Staticman comment submissions

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,55 @@
 
   responsiveNav('.nav-collapse');
 })();
+
+
+
+// Static comments
+(function($) {
+  var $comments = $(".js-comments");
+
+  $("#comments").submit(function() {
+    var form = this;
+
+    $(form).addClass("disabled");
+    $("#comment-form-submit")
+      .html("Loading..");
+
+    $.ajax({
+      type: $(this).attr("method"),
+      url: $(this).attr("action"),
+      data: $(this).serialize(),
+      contentType: "application/x-www-form-urlencoded",
+      success: function(data) {
+        $("#comment-form-submit")
+          .html("Submitted")
+          .addClass("btn--disabled");
+        $("#comments .js-notice")
+          .removeClass("notice--danger")
+          .addClass("notice--success");
+        showAlert(
+          '<strong>Thanks for your comment!</strong> It will show on the site shortly.'
+        );
+       document.getElementById("comments").reset();
+      },
+      error: function(err) {
+        console.log(err);
+        $("#comment-form-submit").html("Submit");
+        $("#comments .js-notice")
+          .removeClass("notice--success")
+          .addClass("notice--danger");
+        showAlert(
+          "<strong>Sorry, there was an error with your submission.</strong> Please make sure all required fields have been completed and try again."
+        );
+        $(form).removeClass("disabled");
+      }
+    });
+
+    return false;
+  });
+
+  function showAlert(message) {
+    $("#comments .js-notice").removeClass("hidden");
+    $("#comments .js-notice-text").html(message);
+  }
+})(jQuery);
